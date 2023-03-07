bff35bb981e5ec655a367aabc29feef329a1a55a
Hung <hunghx@rikkeisoft.com>
2023-03-07 15:06:04 +0700

    update bai thuc hanh

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
 import React, { Component } from "react";
-
 export default class App extends Component {
   constructor(props) {
     super(props);
-    //Khởi tạo 1 state là textInput có giá trị khởi tạo là "" để lưu giá trị người dùng nhập từ ô input
+    //Khởi tạo các state
     this.state = {
-      textInput: "",
+      color: "black",
+      isShow: true,
+      fontSize: 12,
+      content: "helo babi :v!",
     };
     // Khai báo phương thức để sử dụng từ khoá this
-    this.handleChangeInputValue = this.handleChangeInputValue.bind(this);
-    this.handleSubmitForm = this.handleSubmitForm.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleChangeBackground = this.handleChangeBackground.bind(this);
   }
-  // khởi tạo phương thức để cập nhật giá trị người dùng nhập vào ô input vào lưu vào biến textInput
-  handleChangeInputValue(event) {
+  // Phương thức handleToggle sẽ thay đổi giá trị state isShow từ đó ẩn hiện các phần tử
+  handleToggle() {
     this.setState({
-      textInput: event.target.value,
+      isShow: !this.state.isShow,
     });
   }
-  // Khởi tạo phương thức thức khi có sự kiện submit form
-  handleSubmitForm(event) {
-    event.preventDefault();
-    alert("Bạn vừa nhập : '" + this.state.textInput + "'");
+  //Phương thức handleChangeBackground để thay đổi màu sắc với cỡ chữ của đoạn văn bản
+  handleChangeBackground() {
+    this.setState({
+      color: this.state.color === "black" ? "pink" : "black",
+      fontSize: this.state.fontSize === 12 ? 14 : 12,
+      content:
+        this.state.content === "helo babi :v!"
+          ? "U look so beautiful, honey!"
+          : "helo babi :v!",
+    });
   }
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmitForm}>
-          <input
-            value={this.state.textInput}
-            onChange={this.handleChangeInputValue}
-          />
-          <button type="submit">Submit</button>
-        </form>
+        {/*Sử dụng toán tử 3 ngôi để ẩn hiện đoạn văn và nút thay đổi định dạng của đoạn văn*/}
+        {this.state.isShow ? (
+          <>
+            <p
+              style={{ color: this.state.color, fontSize: this.state.fontSize }}
+            >
+              {this.state.content}
+            </p>
+            <button onClick={this.handleChangeBackground}>Change</button>
+          </>
+        ) : (
+          ""
+        )}
+        <button onClick={this.handleToggle}>Toggle</button>
       </div>
     );
   }
